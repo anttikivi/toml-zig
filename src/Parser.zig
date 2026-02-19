9e0055bd99e655162bb0bedb41cc21eb0fc06d02
Antti Kivi <antti@anttikivi.com>
2026-02-19 10:45:11 +0200

    feat: add parsing for table headers

--- src/Parser.zig
+++ src/Parser.zig
@@ -1,5 +1,6 @@
 const Parser = @This();
 
+const build_options = @import("build_options");
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
@@ -9,14 +10,125 @@ const DecodeOptions = @import("decoder.zig").DecodeOptions;
 const Diagnostics = @import("decoder.zig").Diagnostics;
 const Scanner = @import("Scanner.zig");
 const Token = @import("Scanner.zig").Token;
+const value = @import("value.zig");
+const Date = @import("value.zig").Date;
+const Datetime = @import("value.zig").Datetime;
+const HashIndex = @import("value.zig").HashIndex;
+const Time = @import("value.zig").Time;
 
 arena: Allocator,
 scanner: Scanner,
 diagnostics: ?*Diagnostics = null,
 
-const Error = Scanner.Error || error{ Utf8CannotEncodeSurrogateHalf, CodepointTooLarge };
+const Error = Scanner.Error || error{
+    Utf8CannotEncodeSurrogateHalf,
+    CodepointTooLarge,
+} || error{ ExtendedInlineTable, RedefinedTable };
 
-const ParsingTable = struct {};
+const ParsingFlag = struct {
+    inlined: bool = false,
+    standard: bool = false,
+    explicit: bool = false,
+};
+
+const ParsingValue = struct {
+    flag: ParsingFlag = .{},
+    value: union(enum) {
+        string: []const u8,
+        int: i64,
+        float: f64,
+        bool: bool,
+        datetime: Datetime,
+        local_datetime: Datetime,
+        local_date: Date,
+        local_time: Time,
+        array: ArrayList(ParsingValue),
+        table: ParsingTable,
+    },
+};
+
+const ParsingEntry = struct {
+    key: []const u8,
+    value: ParsingValue,
+};
+
+const ParsingTable = struct {
+    entries: ArrayList(ParsingEntry) = .empty,
+    flag: ParsingFlag = .{},
+    index: ?Index = null,
+
+    const Table = @This();
+    const Index = HashIndex(ParsingEntry);
+
+    fn ensureIndex(self: *Table, gpa: Allocator) Allocator.Error!void {
+        assert(self.index == null);
+
+        if (self.entries.items.len < build_options.table_index_threshold) {
+            return;
+        }
+
+        var capacity = build_options.min_index_capacity;
+        while (capacity < self.entries.items.len * 2) {
+            capacity *= 2;
+        }
+
+        self.index = try Index.init(gpa, self.entries.items, capacity);
+    }
+
+    fn getPtr(self: *Table, key: []const u8) ?*ParsingValue {
+        if (self.index) |index| {
+            if (index.lookup(self.entries.items, key)) |i| {
+                return &self.entries.items[i].value;
+            }
+
+            return null;
+        }
+
+        for (self.entries.items) |entry| {
+            if (std.mem.eql(u8, entry.key, key)) {
+                return &entry.value;
+            }
+        }
+
+        return null;
+    }
+
+    fn growIfNeeded(self: *Table, gpa: Allocator) Allocator.Error!void {
+        if (self.index) |*index| {
+            const used = self.entries.items.len;
+            const capacity = index.mask + 1;
+
+            if (used * 2 <= capacity) {
+                return;
+            }
+
+            const new_capacity = capacity * 2;
+            const new_index = try Index.init(gpa, self.entries.items, new_capacity);
+            index.deinit(gpa);
+            self.index = new_index;
+        }
+    }
+
+    fn put(self: *Table, gpa: Allocator, key: []const u8, val: ParsingValue) Allocator.Error!void {
+        try self.entries.append(gpa, .{ .key = key, .value = val });
+
+        if (self.index) |*index| {
+            try self.growIfNeeded(gpa);
+
+            const i = self.entries.items.len - 1;
+            const hash = std.hash.Wyhash.hash(0, key);
+
+            var bucket = hash & index.mask;
+            while (index.buckets[bucket] != value.empty_bucket) {
+                bucket = (bucket + 1) & index.mask;
+            }
+
+            index.buckets[bucket] = i;
+        } else {
+            self.ensureIndex(gpa);
+        }
+    }
+};
 
 pub fn init(arena: Allocator, gpa: Allocator, input: []const u8, opts: DecodeOptions) Parser {
     return .{
@@ -28,18 +140,63 @@ pub fn init(arena: Allocator, gpa: Allocator, input: []const u8, opts: DecodeOpt
 pub fn parse(self: *Parser) Error!ParsingTable {
     const root: ParsingTable = .{};
     const current: *ParsingTable = &root;
-    _ = current;
 
     while (self.scanner.cursor < self.scanner.input.len) {
         const token = try self.scanner.nextKey();
 
         switch (token) {
             .end_of_file => break,
-            else => error.UnexpectedToken,
+            .line_feed => continue,
+            .left_bracket => current = try self.parseTableHeader(&root),
+            else => self.fail(.{ .@"error" = error.UnexpectedToken }),
         }
     }
 
     return root;
+}
+
+fn parseTableHeader(self: *Parser, root: *ParsingTable) Error!*ParsingTable {
+    const keys = try self.parseKey(try self.scanner.nextKey());
+
+    var tok = try self.scanner.nextKey();
+    if (tok != .right_bracket) {
+        return self.fail(.{ .@"error" = error.UnexpectedToken, .msg = "expected right square bracket" });
+    }
+
+    tok = try self.scanner.nextKey();
+    if (tok != .line_feed and tok != .end_of_file) {
+        return self.fail(.{ .@"error" = error.UnexpectedToken, .msg = "expected newline after table header" });
+    }
+
+    const last_key = keys[keys.len - 1];
+    const table = try self.descendToTable(keys[0 .. keys.len - 1], root, true);
+
+    if (table.getPtr(last_key)) |existing| {
+        switch (existing.value) {
+            .table => |*t| {
+                if (existing.flag.explicit or existing.flag.inlined or !existing.flag.standard) {
+                    return self.fail(.{ .@"error" = error.RedefinedTable });
+                }
+
+                existing.flag.explicit = true;
+                return t;
+            },
+            else => return self.fail(.{ .@"error" = error.RedefinedTable }),
+        }
+    }
+
+    if (table.flag.inlined) {
+        return self.fail(.{ .@"error" = error.ExtendedInlineTable });
+    }
+
+    const new_table: ParsingTable = .{ .flag = .{ .standard = true, .explicit = true } };
+    try table.put(self.arena, try self.arena.dupe(u8, last_key), .{
+        .flag = .{ .standard = true, .explicit = true },
+        .value = .{ .table = new_table },
+    });
+
+    const ptr = table.getPtr(last_key).?;
+    return &ptr.value.table;
 }
 
 fn parseKey(self: *Parser, first: Token) Error![][]const u8 {
@@ -73,6 +230,46 @@ fn parseKey(self: *Parser, first: Token) Error![][]const u8 {
     }
 
     return parts.toOwnedSlice(self.arena);
+}
+
+fn descendToTable(self: *Parser, keys: [][]const u8, root: *ParsingTable, is_standard: bool) Error!*ParsingTable {
+    var table = root;
+
+    for (keys) |key| {
+        if (table.getPtr(key)) |existing| {
+            switch (existing.value) {
+                .table => |*t| table = t,
+                .array => |*arr| {
+                    if (arr.items.len == 0) {
+                        return self.fail(.{ .@"error" = error.UnexpectedToken });
+                    }
+
+                    const last = &arr[arr.items.len - 1];
+                    switch (last.value) {
+                        .table => |*t| table = t,
+                        else => return self.fail(.{ .@"error" = error.RedefinedTable }),
+                    }
+                },
+                // TODO: Not so sure which error to choose here, but that's what
+                // the UnexpectedToken is for.
+                else => return self.fail(.{ .@"error" = error.UnexpectedToken }),
+            }
+        } else {
+            // We need to create the intermediate table.
+            var new_table: ParsingTable = .{};
+            new_table.flag.standard = is_standard;
+
+            try table.put(self.arena, try self.arena.dupe(u8, key), .{
+                .flag = .{ .standard = is_standard },
+                .value = .{ .table = new_table },
+            });
+
+            const ptr = table.getPtr(key).?;
+            table = &ptr.value.table;
+        }
+    }
+
+    return table;
 }
 
 fn normalizeString(self: *Parser, s: []const u8, multiline: bool) Error![]const u8 {
@@ -201,7 +398,9 @@ fn fail(self: *const Parser, opts: struct { @"error": Error, msg: ?[]const u8 = 
 
     if (self.diagnostics) |d| {
         const msg = if (opts.msg) |m| m else switch (opts.@"error") {
+            error.ExtendedInlineTable => "extended inline table",
             error.InvalidEscapeSequence => "invalid escape sequence",
+            error.RedefinedTable => "invalid redefinition of table",
             error.UnexpectedToken => "unexpected token",
             error.Reported => unreachable,
         };
