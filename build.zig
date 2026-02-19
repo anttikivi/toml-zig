777ce63dab54116af05a8b0f0f3432a9bfb73fbb
Antti Kivi <antti@anttikivi.com>
2026-02-19 10:45:11 +0200

    build: add minimum index capacity and the threshold for creating the index as build options

--- build.zig
+++ build.zig
@@ -1,25 +1,40 @@
 const std = @import("std");
 
-const Options = struct {
-    optimize: std.builtin.OptimizeMode,
-    target: std.Build.ResolvedTarget,
-};
-
 pub fn build(b: *std.Build) void {
-    const options: Options = .{
-        .optimize = b.standardOptimizeOption(.{}),
-        .target = b.standardTargetOptions(.{}),
-    };
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const min_index_capacity = b.option(
+        u32,
+        "min-index-capacity",
+        "Minimum capacity to initially allocate for the TOML hash table indices",
+    ) orelse 16;
+    const table_index_threshold = b.option(
+        u32,
+        "table-index-threshold",
+        "Threshold for the parsed TOML tables for switching from linear lookup to hashes. Must be a power of 2",
+    ) orelse 64;
+
+    const options = b.addOptions();
+    options.addOption(u32, "min_index_capacity", min_index_capacity);
+    options.addOption(u32, "table_index_threshold", table_index_threshold);
+
+    const toml_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    toml_mod.addOptions("build_options", options);
 
     // Add the library to the package's module set
-    b.modules.put("toml", addTomlMod(b, options)) catch @panic("OOM");
+    b.modules.put("toml", toml_mod) catch @panic("OOM");
 
     const test_step = b.step("test", "Run all of the tests");
 
     // Unit tests
     {
         const step = b.step("test-unit", "Run the unit tests");
-        const tests = b.addTest(.{ .root_module = addTomlMod(b, options) });
+        const tests = b.addTest(.{ .root_module = toml_mod });
         step.dependOn(&b.addRunArtifact(tests).step);
         test_step.dependOn(step);
     }
@@ -35,14 +50,4 @@ pub fn build(b: *std.Build) void {
         step.dependOn(&b.addFmt(.{ .paths = fmt_include_paths, .check = true }).step);
         test_step.dependOn(step);
     }
-}
-
-fn addTomlMod(b: *std.Build, opts: Options) *std.Build.Module {
-    const mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = opts.target,
-        .optimize = opts.optimize,
-    });
-
-    return mod;
 }
