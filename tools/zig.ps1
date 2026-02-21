1905f6b7cc564c4ac667d8ab363e1a29fc4e5a99
Antti Kivi <antti@anttikivi.com>
2026-02-21 22:23:35 +0200

    fix: fix the Zig script name on non-Windows platforms

--- tools/zig.ps1
+++ tools/zig.ps1
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo `# <#` >/dev/null 2>&1
 SCRIPT_DIR="$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)"
-exec "${SCRIPT_DIR}/zig.sh" "$@"
+exec "${SCRIPT_DIR}/zig" "$@"
 exit
 #> > $null
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
