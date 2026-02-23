13f06e67545f085b0b0275d1524c7c4d7d1f50a5
Antti Kivi <antti@anttikivi.com>
2026-02-23 18:50:31 +0200

    fix: fix wrong operator when checking if minisign can be installed on Darwin

--- tools/download_minisign.sh
+++ tools/download_minisign.sh
@@ -116,7 +116,7 @@ main() {
         _arch="x86_64"
     fi
 
-    if [ "$(uname)" = "Darwin" ] || [ "${_arch}" = "x86_64" ]; then
+    if [ "$(uname)" = "Darwin" ] && [ "${_arch}" = "x86_64" ]; then
         echo "minisign repository does not provide prebuilt binaries for x86_64 Darwin" >&2
         echo "please install minisign using other methods, like Homebrew" >&2
         return 1
