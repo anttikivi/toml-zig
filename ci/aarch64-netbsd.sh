3f06c87c42d098d7da1f65f951950726056c3645
anttikivi <anttikivi@noreply.codeberg.org>
2026-02-26 15:48:16 +0100

    ci: add script for aarch64 NetBSD

---
+++ ci/aarch64-netbsd.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eux
+
+sudo -n pkgin -y install go minisign
+
+if ! sudo -n pkgin -y install mozilla-rootcerts; then
+    sudo -n pkgin -y install ca-certificates
+fi
+
+if command -v certctl >/dev/null 2>&1; then
+    sudo -n certctl rehash
+fi
+
+if [ ! -f /etc/openssl/certs/ca-certificates.crt ] && [ -f /usr/pkg/etc/openssl/certs/ca-certificates.crt ]; then
+    sudo -n mkdir -p /etc/openssl/certs
+    sudo -n ln -sf /usr/pkg/etc/openssl/certs/ca-certificates.crt /etc/openssl/certs/ca-certificates.crt
+fi
+
+./zig build fetch-toml-test
+./zig build test -Dtoml-test-timeout=10s --summary all
