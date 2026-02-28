edfd580974033771096d72dda53d4d107e8803ab
Antti Kivi <antti@anttikivi.com>
2026-02-28 14:19:54 +0200

    refactor(bench): extract the key-value pair generation into separate functions

--- test/generate_bench_data.zig
+++ test/generate_bench_data.zig
@@ -79,68 +79,90 @@ fn generateFlatKv(gpa: Allocator, target_size: usize) ![]const u8 {
     var i: usize = 0;
     while (result.items.len < target_size) : (i += 1) {
         if (bench_options.random_bench) {
-            switch (prng.random().uintAtMost(u8, 4)) {
-                0 => {
-                    try appendRandomString(gpa, rand, &result, 3, 20);
-                    try appendPrint(gpa, &result, "_{d} = \"", .{i});
-                    try appendRandomString(gpa, rand, &result, 3, 20);
-                    try result.appendSlice(gpa, "\"\n");
-                },
-                1 => {
-                    try appendRandomString(gpa, rand, &result, 3, 20);
-                    try appendPrint(gpa, &result, "_{d} = ", .{i});
-                    if (rand.uintAtMost(u32, 100_000) % 2 == 0) {
-                        try result.appendSlice(gpa, "true\n");
-                    } else {
-                        try result.appendSlice(gpa, "false\n");
-                    }
-                },
-                2 => {
-                    try appendRandomString(gpa, rand, &result, 3, 20);
-                    switch (rand.int(u3)) {
-                        0 => try appendPrint(gpa, &result, "_{d} = 0b{b}\n", .{ i, rand.int(u32) }),
-                        1 => try appendPrint(gpa, &result, "_{d} = 0o{o}\n", .{ i, rand.int(u32) }),
-                        2 => try appendPrint(gpa, &result, "_{d} = 0x{x}\n", .{ i, rand.int(u32) }),
-                        else => try appendPrint(gpa, &result, "_{d} = {d}\n", .{ i, rand.int(i64) }),
-                    }
-                },
-                3 => {
-                    try appendRandomString(gpa, rand, &result, 3, 20);
-                    switch (rand.int(u1)) {
-                        0 => try appendPrint(gpa, &result, "_{d} = {e}\n", .{
-                            i,
-                            rand.float(f64) + @as(f64, @floatFromInt(rand.int(u32))),
-                        }),
-                        1 => try appendPrint(gpa, &result, "_{d} = {d}\n", .{
-                            i,
-                            rand.float(f64) + @as(f64, @floatFromInt(rand.int(u32))),
-                        }),
-                    }
-                },
-                4 => {
-                    try appendRandomString(gpa, rand, &result, 3, 20);
-                    try appendPrint(gpa, &result, "_{d} = {d}-{d:0>2}-{d:0>2}T{d:0>2}:{d:0>2}:{d:0>2}\n", .{
-                        i,
-                        rand.intRangeAtMost(u32, 1000, 9999),
-                        rand.intRangeAtMost(u8, 1, 12),
-                        rand.intRangeAtMost(u8, 1, 28),
-                        rand.intRangeAtMost(u8, 0, 23),
-                        rand.intRangeAtMost(u8, 0, 59),
-                        rand.intRangeAtMost(u8, 0, 59),
-                    });
-                },
-                else => unreachable,
-            }
+            const s = try generateKeyValueRandom(gpa, rand, i);
+            defer gpa.free(s);
+            try result.appendSlice(gpa, s);
         } else {
-            switch (i % 5) {
-                0 => try appendPrint(gpa, &result, "string_{d} = \"string_val_{d}\"\n", .{ i, i }),
-                1 => try appendPrint(gpa, &result, "bool_{d} = {s}\n", .{ i, if (i % 2 == 0) "true" else "false" }),
-                2 => try appendPrint(gpa, &result, "int_{d} = {d}\n", .{ i, i * 44 }),
-                3 => try appendPrint(gpa, &result, "float_{d} = {d}\n", .{ i, @as(f64, @floatFromInt(i * 27)) }),
-                4 => try appendPrint(gpa, &result, "dt_{d} = 2024-01-{d:0>2}T12:00:00Z\n", .{ i, (i % 28) + 1 }),
-                else => unreachable,
-            }
+            const s = try generateKeyValueDeterministic(gpa, i);
+            defer gpa.free(s);
+            try result.appendSlice(gpa, s);
         }
+    }
+
+    return result.toOwnedSlice(gpa);
+}
+
+fn generateKeyValueRandom(gpa: Allocator, rand: std.Random, i: usize) ![]const u8 {
+    var result: ArrayList(u8) = .empty;
+    errdefer result.deinit(gpa);
+
+    switch (rand.uintAtMost(u8, 4)) {
+        0 => {
+            try appendRandomString(gpa, rand, &result, 3, 20);
+            try appendPrint(gpa, &result, "_{d} = \"", .{i});
+            try appendRandomString(gpa, rand, &result, 3, 20);
+            try result.appendSlice(gpa, "\"\n");
+        },
+        1 => {
+            try appendRandomString(gpa, rand, &result, 3, 20);
+            try appendPrint(gpa, &result, "_{d} = ", .{i});
+            if (rand.uintAtMost(u32, 100_000) % 2 == 0) {
+                try result.appendSlice(gpa, "true\n");
+            } else {
+                try result.appendSlice(gpa, "false\n");
+            }
+        },
+        2 => {
+            try appendRandomString(gpa, rand, &result, 3, 20);
+            switch (rand.int(u3)) {
+                0 => try appendPrint(gpa, &result, "_{d} = 0b{b}\n", .{ i, rand.int(u32) }),
+                1 => try appendPrint(gpa, &result, "_{d} = 0o{o}\n", .{ i, rand.int(u32) }),
+                2 => try appendPrint(gpa, &result, "_{d} = 0x{x}\n", .{ i, rand.int(u32) }),
+                else => try appendPrint(gpa, &result, "_{d} = {d}\n", .{ i, rand.int(i64) }),
+            }
+        },
+        3 => {
+            try appendRandomString(gpa, rand, &result, 3, 20);
+            switch (rand.int(u1)) {
+                0 => try appendPrint(gpa, &result, "_{d} = {e}\n", .{
+                    i,
+                    rand.float(f64) + @as(f64, @floatFromInt(rand.int(u32))),
+                }),
+                1 => try appendPrint(gpa, &result, "_{d} = {d}\n", .{
+                    i,
+                    rand.float(f64) + @as(f64, @floatFromInt(rand.int(u32))),
+                }),
+            }
+        },
+        4 => {
+            try appendRandomString(gpa, rand, &result, 3, 20);
+            try appendPrint(gpa, &result, "_{d} = {d}-{d:0>2}-{d:0>2}T{d:0>2}:{d:0>2}:{d:0>2}\n", .{
+                i,
+                rand.intRangeAtMost(u32, 1000, 9999),
+                rand.intRangeAtMost(u8, 1, 12),
+                rand.intRangeAtMost(u8, 1, 28),
+                rand.intRangeAtMost(u8, 0, 23),
+                rand.intRangeAtMost(u8, 0, 59),
+                rand.intRangeAtMost(u8, 0, 59),
+            });
+        },
+        else => unreachable,
+    }
+
+    return result.toOwnedSlice(gpa);
+}
+
+fn generateKeyValueDeterministic(gpa: Allocator, i: usize) ![]const u8 {
+    var result: ArrayList(u8) = .empty;
+    errdefer result.deinit(gpa);
+
+    switch (i % 5) {
+        0 => try appendPrint(gpa, &result, "string_{d} = \"string_val_{d}\"\n", .{ i, i }),
+        1 => try appendPrint(gpa, &result, "bool_{d} = {s}\n", .{ i, if (i % 2 == 0) "true" else "false" }),
+        2 => try appendPrint(gpa, &result, "int_{d} = {d}\n", .{ i, i * 44 }),
+        3 => try appendPrint(gpa, &result, "float_{d} = {d}\n", .{ i, @as(f64, @floatFromInt(i * 27)) }),
+        4 => try appendPrint(gpa, &result, "dt_{d} = 2024-01-{d:0>2}T12:00:00Z\n", .{ i, (i % 28) + 1 }),
+        else => unreachable,
     }
 
     return result.toOwnedSlice(gpa);
